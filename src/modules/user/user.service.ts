45f351a7fc8de5795606c1f8941f55c93b4f39bd
lucas5g <lucasdesousa19@hotmail.com>
2023-09-10 12:27:34 -0300

    start module user

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
+
+  constructor(private readonly prisma: PrismaService){}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prisma.u;
   }
 
   findOne(id: number) {
