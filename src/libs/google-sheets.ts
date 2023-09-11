34533d514812c2f2f3571d8e41155d6c600916db
lucas.sousa <lucas.sousa@codificar.com.br>
2023-09-11 19:47:38 -0300

    commnet import googleapi

--- src/libs/google-sheets.ts
+++ src/libs/google-sheets.ts
@@ -1,35 +1,37 @@
 import 'dotenv/config'
 import { NotFoundException } from "@nestjs/common";
-import { google } from "googleapis";
+// import { google } from "googleapis";
 import { env } from "../utils/env";
 
 export async function googleSheets({range}:{range:string}){
-  const auth = new google.auth.GoogleAuth({
-    credentials:{
-      client_id: env.googleClientId,
-      client_email: env.googleClientEmail,
-      private_key: env.googlePrivateKey
-    },
-    scopes: 'https://www.googleapis.com/auth/spreadsheets'
-  })
 
-  const { spreadsheets } = google.sheets({version: 'v4', auth})
+  return []
+  // const auth = new google.auth.GoogleAuth({
+  //   credentials:{
+  //     client_id: env.googleClientId,
+  //     client_email: env.googleClientEmail,
+  //     private_key: env.googlePrivateKey
+  //   },
+  //   scopes: 'https://www.googleapis.com/auth/spreadsheets'
+  // })
 
-  try{
+  // const { spreadsheets } = google.sheets({version: 'v4', auth})
 
-    const { data } = await spreadsheets.values.get({
-      spreadsheetId: env.spreadSheetId,
-      range
-    })
+  // try{
+
+  //   const { data } = await spreadsheets.values.get({
+  //     spreadsheetId: env.spreadSheetId,
+  //     range
+  //   })
     
-    const values:any[] = sheetsToArrayObjects(data.values)
+  //   const values:any[] = sheetsToArrayObjects(data.values)
     
-    return values
-  }catch(error){
-    // console.log(env)
-    console.log(error)
-    throw new NotFoundException('Unidadade não registrada na planilha')
-  }
+  //   return values
+  // }catch(error){
+  //   // console.log(env)
+  //   console.log(error)
+  //   throw new NotFoundException('Unidadade não registrada na planilha')
+  // }
 }
 
 function sheetsToArrayObjects(data: any[][] | undefined | null) {
