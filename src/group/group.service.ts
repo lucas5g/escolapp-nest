e230f7318ab60396155abd5d188d8f6b31378ea8
lucas.sousa <lucas.sousa@codificar.com.br>
2023-09-16 16:24:37 -0300

    start config jest with .env

--- src/group/group.service.ts
+++ src/group/group.service.ts
@@ -12,7 +12,6 @@ export class GroupService {
 
   async findAll({unity}: FindGroupDto) {
     const groups = await googleSheets({range: `${unity}!G:H`}) as {turma:string, quantidade: number}[];
-    console.log(groups.length)
     return groups.map(group => {
       return {
         name: group.turma,
