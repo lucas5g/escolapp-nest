4d3e53124979a04bb5504239082d0f4cae2a3a9f
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-04-18 11:16:10 -0300

    up lib

--- src/main.ts
+++ src/main.ts
@@ -30,9 +30,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  const port = 8000
+  const port = 8000;
   await app.listen(port);
 
-  Logger.log(`Server Run http://localhost:${port}`)
+  Logger.log(`Server Run http://localhost:${port}`);
 }
 bootstrap();
