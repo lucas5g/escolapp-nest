9bfb2ea28725b8e485dbbd0628b099fbc9d86c1e
lucas.assuncao <lucas.assuncao@defensoria.mg.def.br>
2024-04-22 12:22:32 -0300

    add logger places

--- src/prisma/prisma-exception.filter.ts
+++ src/prisma/prisma-exception.filter.ts
@@ -1,4 +1,4 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 import { translate } from '../utils/translate';
 
@@ -7,8 +7,6 @@ export class PrismaExceptionFilter implements ExceptionFilter {
   catch(exception: PrismaClientKnownRequestError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-
-    // console.log(exception);
 
     if (exception.code === 'P2025') {
       return response.status(404).json({
@@ -31,8 +29,11 @@ export class PrismaExceptionFilter implements ExceptionFilter {
       });
     }
 
+    Logger.error(exception)
+
     return response.status(500).json({
       message: 'Internal server error',
+      statusCode: 500
     });
   }
 }
